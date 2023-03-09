af40afe51b6f6fb7163bd42e10a509e9b1475dfc
Ihab El amrani <iel-amra@z3r5p5.42lyon.fr>
2023-03-09 17:15:49 +0100

    Yoloyihi

--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iel-amra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/15 16:00:49 by iel-amra          #+#    #+#             */
-/*   Updated: 2023/03/09 13:52:30 by iel-amra         ###   ########.fr       */
+/*   Created: 2023/03/09 15:38:46 by iel-amra          #+#    #+#             */
+/*   Updated: 2023/03/09 16:02:49 by iel-amra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	CONTACT_HPP
-# define CONTACT_HPP
-
-class Contact
+void	PhoneBook::add()
 {
-private:
-	std::string _data[5];	
+}
 
-public:
-	void	register();
-	void	display();
-	string	get_member(const int	i);
-};
-
-#endif
+void	PhoneBook::search(std::string name)
+{
+}
