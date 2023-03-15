61a28872b753266348be8a6dc0fa3cca5622693d
Ihab El amrani <iel-amra@z2r4p6.42lyon.fr>
2023-03-15 15:01:41 +0100

    VITE

--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: iel-amra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:38:46 by iel-amra          #+#    #+#             */
-/*   Updated: 2023/03/11 16:57:53 by iel-amra         ###   ########.fr       */
+/*   Updated: 2023/03/14 14:39:50 by iel-amra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ PhoneBook::PhoneBook(): _last(-1), _nb(0)
 
 void	PhoneBook::add()
 {
-	_last = (last + 1) % 8;
-	_contacts[last].registering();
+	_last = (_last + 1) % 8;
+	_contacts[_last].registering();
 	_nb++;
 }
 
 void	PhoneBook::display()
 {
-	cout << "Hello";	
+	std::cout << "Hello";
 }
