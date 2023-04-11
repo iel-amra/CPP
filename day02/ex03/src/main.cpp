e23f1f176fd84f65ce505cd5ba8af9979d4bbc95
Ihab El amrani <iel-amra@z3r11p1.42lyon.fr>
2023-04-11 13:43:58 +0200

    Working on day 02

--- day02/ex03/src/main.cpp
+++ day02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iel-amra <iel-amra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 12:56:13 by iel-amra          #+#    #+#             */
-/*   Updated: 2023/04/08 15:57:24 by iel-amra         ###   ########.fr       */
+/*   Updated: 2023/04/11 13:15:10 by iel-amra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ static bool same_side(Point const  &a, Point const &b, Point const &c, Point con
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-    (void) point;
     const Point triangle[3]{a, b, c};
     
     for (int i = 0; i < 3; i++)
@@ -39,6 +38,13 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
         return (false);
     }
     return (true);
+}
+
+static void test(const Point & a, const Point & b, const Point & c, const Point & point)
+{
+    std::cout   << "Triangle is a : " << a << ",b : " << b << ",c : " << c << " and point is "
+                << point << ", bsp returns"
+                << bsp(a, b, c, point) << std::endl;
 }
 
 int main( void ) 
